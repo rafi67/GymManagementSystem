cf4d43520bc2949c1a9a0bec9586df55bcbc5f14
Rafi Siddique <rafisiddique67@gmail.com>
2022-12-25 16:54:59 +0600

    indiviual deletion function added

--- Gym/GymManagement/main.cpp
+++ Gym/GymManagement/main.cpp
@@ -1,33 +1,37 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <limits>
 #define Max 1000000
 
 using namespace std;
 
 struct GymMember {
+    int id;
     string name;
     string address;
     string phone;
     string payInfo;
 };
 
-int cnt = 1, in = 0;
+int cnt = 1, in = 0, ID = 1;
 
 GymMember G[Max];
 
 void addMemeber() {
     string name, address, phone, info;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
     cout << "Enter name:\n";
-    cin >> name;
+    getline(cin, name);
     cout << "Enter Address:\n";
-    cin >> address;
+    getline(cin, address);
     cout << "Enter Phone Number:\n";
     cin >> phone;
     cout << "Enter Payment Info:\n";
-    cin >> info;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    getline(cin, info);
     ofstream file("gymMember.dat", ios::out|ios::app);
-    file << name << " " << address << " " << phone << " " << info << "\n";
+    file << ID++ << "*" << name << "*" << address << "*" << phone << " " << info << "\n";
     cout << "Data stored\n";
     file.close();
 }
@@ -37,20 +41,32 @@ void StringSeperator(string Data) {
 
     for(int i=0; i<s; ++i) {
         if(cnt1==0) {
-            if(Data[i]!=' ') G[in].name += Data[i];
+            G[i].id = 0;
+            if(Data[i]!='*') {
+                G[i].id *= 10;
+                G[i].id += Data[i]-'0';
+            }
             else {
                 ++cnt1;
+                ID = G[i].id+1;
                 continue;
             }
         }
         else if(cnt1==1) {
-            if(Data[i]!=' ') G[in].address += Data[i];
+            if(Data[i]!='*') G[in].name += Data[i];
             else {
                 ++cnt1;
                 continue;
             }
         }
         else if(cnt1==2) {
+            if(Data[i]!='*') G[in].address += Data[i];
+            else {
+                ++cnt1;
+                continue;
+            }
+        }
+        else if(cnt1==3) {
             if(Data[i]!=' ') G[in].phone += Data[i];
             else {
                 ++cnt1;
@@ -90,12 +106,42 @@ void display() {
         "Payment Information: " << G[i].payInfo << "\n\n";
 }
 
-void Delet_data() {
+void Delete_data() {
     ofstream file("GymMember.dat");
-    if(file.fail()) cout << "unable to delete data\n";
+    if(file.fail()) cout << "unable to delete data file not found\n";
     else {
         file.close();
         cout << "Data deleted successfully\n";
+    }
+}
+
+int Linear_Search(string n) {
+    for(int i=0; i<cnt-2; ++i) if(G[i].name==n) return i;
+    return -1;
+}
+
+void Delete_Seperately() {
+    string name;
+    cout << "Enter the name to delete: ";
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    getline(cin, name);
+    if(cnt==1) dataRetrive();
+    int index = Linear_Search(name);
+    if(index==-1) cout << "Deletion failed, Data not found\n";
+    else {
+        for(int i=index; i<cnt-3; ++i) {
+            G[i].id = G[i+1].id;
+            G[i].name = G[i+1].name;
+            G[i].address = G[i+1].address;
+            G[i].phone = G[i+1].phone;
+            G[i].payInfo = G[i+1].payInfo;
+        }
+        --cnt;
+        ofstream file("gymMember.dat");
+        for(int i=0; i<cnt-2; ++i) file << G[i].id << "*" << G[i].name
+        << "*" << G[i].address << "*" << G[i].phone << " " << G[i].payInfo;
+        file.close();
+        cout << "Successfully deleted member " << name << "\n";
     }
 }
 
@@ -106,7 +152,7 @@ int main() {
 
     while(1) {
         cout << "Add member press 1\nShow all member's info press 2\n" <<
-        "Delete all member's info press 3\nExit press 4\n";
+        "Delete all member's info press 3\nDelete member indiviually press 4\nExit press 5\n";
         cin >> c;
         switch(c) {
             case 1:
@@ -117,9 +163,12 @@ int main() {
                 display();
                 break;
             case 3:
-                Delet_data();
+                Delete_data();
                 break;
             case 4:
+                Delete_Seperately();
+                break;
+            case 5:
                 exit(0);
             default:
                 cout << "Wrong Input\n";
